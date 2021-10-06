a9549553f417aec70e6c8a9732227b03105c68c7
Matti Saarinen <mjsaarin@cc.helsinki.fi>
2021-10-06 13:04:11 +0300

    Added a configuration option allow_nonincreasing that allows collecting a set of OIDs that aren't in a strictly increasing order.
    Signed-off-by: Matti Saarinen <mjsaarin@cc.helsinki.fi>

--- config/config.go
+++ config/config.go
@@ -50,6 +50,7 @@ var (
 		Timeout:                 time.Second * 5,
 		Auth:                    DefaultAuth,
 		UseUnconnectedUDPSocket: false,
+		AllowNonIncr:   false,
 	}
 	DefaultModule = Module{
 		WalkParams: DefaultWalkParams,
@@ -69,6 +70,7 @@ type WalkParams struct {
 	Timeout                 time.Duration `yaml:"timeout,omitempty"`
 	Auth                    Auth          `yaml:"auth,omitempty"`
 	UseUnconnectedUDPSocket bool          `yaml:"use_unconnected_udp_socket,omitempty"`
+	AllowNonIncr            bool          `yaml:"allow_nonincreasing,omitempty"`
 }
 
 type Module struct {
